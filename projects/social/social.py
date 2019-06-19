de829abd0cb9d0a31724903ff93bf42c7b067cdd
Jacob McFaul <mcjakedude@gmail.com>
2019-06-19 13:53:41 -0700

    initial commit and some code

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,8 +1,32 @@
+class Queue:
+    def __init__(self):
+        self.queue = []
 
+# ---------------------------------------------------------------------------
+
+    def enqueue(self, value):   
+        self.queue.append(value)
+
+# ---------------------------------------------------------------------------
+
+    def dequeue(self):
+        if (self.size) > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+# ---------------------------------------------------------------------------
+
+    def size(self):
+        return len(self.queue)
+
+# ==========================================================================
 
 class User:
     def __init__(self, name):
         self.name = name
+
+# ==========================================================================
 
 class SocialGraph:
     def __init__(self):
@@ -10,10 +34,9 @@ class SocialGraph:
         self.users = {}
         self.friendships = {}
 
+# ---------------------------------------------------------------------------
+
     def addFriendship(self, userID, friendID):
-        """
-        Creates a bi-directional friendship
-        """
         if userID == friendID:
             print("WARNING: You cannot be friends with yourself")
         elif friendID in self.friendships[userID] or userID in self.friendships[friendID]:
@@ -22,33 +45,30 @@ class SocialGraph:
             self.friendships[userID].add(friendID)
             self.friendships[friendID].add(userID)
 
+# ---------------------------------------------------------------------------
+
     def addUser(self, name):
-        """
-        Create a new user with a sequential integer ID
-        """
-        self.lastID += 1  # automatically increment the ID to assign the new user
+        self.lastID += 1
         self.users[self.lastID] = User(name)
         self.friendships[self.lastID] = set()
 
+# ---------------------------------------------------------------------------
+
     def populateGraph(self, numUsers, avgFriendships):
-        """
-        Takes a number of users and an average number of friendships
-        as arguments
-
-        Creates that number of users and a randomly distributed friendships
-        between those users.
-
-        The number of users must be greater than the average number of friendships.
-        """
-        # Reset graph
         self.lastID = 0
         self.users = {}
         self.friendships = {}
-        # !!!! IMPLEMENT ME
+        for i in range(0, numUsers):
+            self.addUser(f"User {i}")
+        possibleFriendships = []
+        for userID in self.users:
+            for friendID in range(userID +1, self.lastID +1):
+                possibleFriendships.append((userID, friendID))
+        random.shuffle(possibleFriendships)
+        x = 0
+        for i in range(0, (numUsers * avgFriendships // 2))
 
-        # Add users
-
-        # Create friendships
+# ---------------------------------------------------------------------------
 
     def getAllSocialPaths(self, userID):
         """
@@ -63,6 +83,7 @@ class SocialGraph:
         # !!!! IMPLEMENT ME
         return visited
 
+# ---------------------------------------------------------------------------
 
 if __name__ == '__main__':
     sg = SocialGraph()
