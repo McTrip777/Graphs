6989e0ed126daa41b6d396a97fe4b37688c2d5a1
Jacob McFaul <mcjakedude@gmail.com>
2019-06-20 13:33:39 -0700

    starting basic bft code

--- projects/social/social.py
+++ projects/social/social.py
@@ -3,41 +3,29 @@ import random
 class Queue:
     def __init__(self):
         self.queue = []
-
 # ---------------------------------------------------------------------------
-
     def enqueue(self, value):   
         self.queue.append(value)
-
 # ---------------------------------------------------------------------------
-
     def dequeue(self):
         if (self.size()) > 0:
             return self.queue.pop(0)
         else:
             return None
-
 # ---------------------------------------------------------------------------
-
     def size(self):
         return len(self.queue)
-
 # ==========================================================================
-
 class User:
     def __init__(self, name):
         self.name = name
-
 # ==========================================================================
-
 class SocialGraph:
     def __init__(self):
         self.lastID = 0
         self.users = {}
         self.friendships = {}
-
 # ---------------------------------------------------------------------------
-
     def addFriendship(self, userID, friendID):
         if userID == friendID:
             print("WARNING: You cannot be friends with yourself")
@@ -46,16 +34,12 @@ class SocialGraph:
         else:
             self.friendships[userID].add(friendID)
             self.friendships[friendID].add(userID)
-
 # ---------------------------------------------------------------------------
-
     def addUser(self, name):
         self.lastID += 1
         self.users[self.lastID] = User(name)
         self.friendships[self.lastID] = set()
-
 # ---------------------------------------------------------------------------
-
     def populateGraph(self, numUsers, avgFriendships):
         self.lastID = 0
         self.users = {}
@@ -70,9 +54,7 @@ class SocialGraph:
         for i in range(0, (numUsers * avgFriendships // 2)):
             friendship = possibleFriendships[i]
             self.addFriendship(friendship[0], friendship[1])
-
 # ---------------------------------------------------------------------------
-
     def getAllSocialPaths(self, userID):
         visited = {}
         q = Queue()
@@ -88,9 +70,7 @@ class SocialGraph:
                         new_path.append(friendID)
                         q.enqueue(new_path)
         return visited
-
 # ---------------------------------------------------------------------------
-
 if __name__ == '__main__':
     sg = SocialGraph()
     sg.populateGraph(10, 2)
